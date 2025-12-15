1b0f4480dd317699b90c0a5b2354dc8b43436235
Chase Harrison <ChaseH88@gmail.com>
2025-12-14 21:42:29 -0600

    add front end and test

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.ts"],
-  moduleFileExtensions: ["ts", "js"],
+  testMatch: ["**/src/**/__tests__/**/*.test.ts"],
+  verbose: true,
+  transform: {
+    "^.+\\.ts$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+  },
+  moduleFileExtensions: ["ts", "js", "json"],
 };
